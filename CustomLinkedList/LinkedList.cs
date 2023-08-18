a920c183f92f0c16618e528180dc0f46286ebe4e
shepherrrd <shefkid2@gmail.com>
2023-08-18 17:44:23 +0100

    Add project files.

---
+++ CustomLinkedList/LinkedList.cs
@@ -0,0 +1,74 @@
+﻿namespace CustomLinkedList;
+
+public class LinkedList <T>
+{
+    public Node<T> First { get; private set; }
+    public Node<T> Last { get; private set; }
+    public int Count { get; private set; }
+
+    public LinkedList()
+    {
+        First = null;
+        Last = null;
+    }
+
+    public void AddFirst(Node<T> node)
+    {
+        if(First == null)
+        {
+            First = node;
+            Last = node;
+        }
+        else
+        {
+            node.Next = First;
+            First = node;
+        }
+        Count++;
+    }
+    public void AddLast(Node<T> node)
+    {
+        if(First == null)
+        {
+            First = node;
+            Last = node;
+        }
+        else
+        {
+            Last.Next = node;
+            Last = node;
+        }
+        Count++;
+    }
+    public void AddAfter(Node<T> newnode, Node<T> existingNode) {
+    
+    if(Last == existingNode)
+        {
+            Last = newnode;
+        }
+        newnode.Next = existingNode.Next;
+        existingNode.Next = newnode;
+        Count++;
+    }
+    public Node<T> Find(T target)
+    {
+        Node<T> node = First;
+        while(node != null && !node.Data!.Equals(target)) {        
+        node = node.Next;
+        }
+        return node;
+    }
+    public void RemoveFirst()
+    {
+        if(First is null || Count is 0)
+        {
+            return;
+        }
+        First = First.Next;
+        Count--;
+    }
+    public void Remove(Node<T> removedNode)
+    {
+
+    }
+}
---
+++ CustomLinkedList/Node.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomLinkedList;
+
+public class Node<T>
+{
+     public T Data { get; set; }
+     public   Node<T> Next { get; internal set; }
+    public Node(T data)
+    {
+        Data = data; 
+    }
+}
